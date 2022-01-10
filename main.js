a9d12260b69ba177d819ca9479bb4f756d467c20
gitraspigner <gitraspigner@gmail.com>
2022-01-10 14:34:15 -0800

    Adding starter files for this shader project, who knows what awaits!

---
+++ main.js
@@ -0,0 +1,74 @@
+let theShader;
+
+function preload() {
+
+  // load shader
+  theShader = getShader(this._renderer);
+}
+
+function setup() {
+  // shaders require WEBGL
+  // vars for fullscreen in browser:
+  // windowWidth, WindowHeight
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  // shader() sets the active shader with our shader
+  shader(theShader);
+  //noStroke();
+}
+
+function draw() {
+  // send resulotion of sketch into shader
+  theShader.setUniform('u_resolution', [width, height]);
+
+  // set the active shader with our
+
+
+  // rect gives us some geometry on the screen
+  rect(0, 0, width, height);
+
+}
+
+//function windowResized() new Promise(function(resolve, reject) {
+//  resizeCanvas(windowWidth, windowHeight);
+//});
+
+
+
+
+/*
+
+let metaballShader;
+
+const N_balls = 20,
+			metaballs = [];
+
+function preload() {
+	metaballShader = getShader(this._renderer);
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL);
+	shader(metaballShader);
+
+	for (let i = 0; i < N_balls; i ++) metaballs.push(new Metaball());
+}
+
+function draw() {
+	var data = [];
+
+	for (const ball of metaballs) {
+		ball.update();
+		data.push(ball.pos.x, ball.pos.y, ball.radius);
+	}
+
+	metaballShader.setUniform("metaballs", data);
+	rect(0, 0, width, height);
+}
+
+// OpenProcessing has a bug where it always creates a scrollbar on Chromium.
+function mouseWheel() { // This stops the canvas from scrolling by a few pixels.
+	return false;
+}
+
+*/
---
+++ shader.js
@@ -0,0 +1,42 @@
+function getShader(_renderer) {
+	const vert = `
+    // shapes are driven by p5
+    // our vertex data from WEBGL/p5
+    attribute vec3 aPosition;
+
+    void main() {
+      // copy the position data into a vec4
+      // (x,y,z,w). We are not using z, because we are only operating
+      // in two dimensions, x and y. We will put in 1.0 as the w
+      // parameter (when w = 1.0 the vector is treated as a position,
+      // when w = 0.0 the vector is treated as a direction, this is standard vector math.
+      vec4 positionVec4 = vec4(aPosition, 1.0);
+
+
+      positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+
+      // send the vertex information to the frag shader
+      gl_Position = positionVec4;
+    }
+  `;
+
+	const frag = `
+    // frag needs this to run natively, dont ask why
+    #ifdef GL_ES
+    precision mediump float;
+    #endif
+
+    uniform vec2 u_resolution; // this is passed in as uniform from main.js
+
+    void main () {
+
+      // gl_FragCoord are the window relative coordinates
+      // of the current fragment
+
+      vec2 st = gl_FragCoord.xy/u_resolution.xy
+      gl_FragColor = vec4(st.x, 0.0, 0.0, 1.0);
+    }
+	`;
+
+	return new p5.Shader(_renderer, vert, frag);
+}
